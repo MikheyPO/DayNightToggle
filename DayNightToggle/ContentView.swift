d2053abb5557c41cd95b8eafbe0b88d2def2582c
mikhey <mikhey.work@gmail.com>
2024-02-26 16:06:06 -0800

    init

---
+++ DayNightToggle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayNightToggle
+//
+//  Created by mikhey on 2024-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayNightToggle/DayNightToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayNightToggleApp.swift
+//  DayNightToggle
+//
+//  Created by mikhey on 2024-02-26.
+//
+
+import SwiftUI
+
+@main
+struct DayNightToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
